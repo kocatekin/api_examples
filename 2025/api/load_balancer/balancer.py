9d45afa53faa494dc499cf48ac8b4cd72ce69938
kocatekin <116370462+kocatekin@users.noreply.github.com>
2025-03-24 21:52:25 +0300

    Create balancer.py

---
+++ 2025/api/load_balancer/balancer.py
@@ -0,0 +1,24 @@
+from flask import Flask
+import requests as req
+
+app = Flask(__name__)
+
+SERVERS = ["http://localhost:5001","http://localhost:5002","http://localhost:5003"]
+
+current_server_idx = 0
+
+def get_next_server():
+  global current_server_idx
+  current_server_idx = (current_server_idx + 1) % len(SERVERS)
+  return current_server_idx
+
+
+@app.route('/')
+def index():
+  res = req.get(SERVERS[get_next_server()])
+  return res.text
+
+
+if __name__ == "__main__":
+  app.run(port=5000)
+  
